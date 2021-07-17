9ecd24516626f8d7de30cbfb8d6a883965b76793
Nexusov <egor2001grashin@gmail.com>
2021-07-17 20:52:29 +0300

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+	return (
+		<header className="header">
+			<img src="https://play-lh.googleusercontent.com/Ri6xV_kcCS4PUduDTt2-g3i0rx5o2w7yOHzubc93WRD2AB7GKm9Iqg1hCY3SFox3aQ" />
+		</header>
+	);
+};
+
+export default Header;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function Profile() {
+	return (
+		<div>
+			<div className="content">
+				<div>
+					<img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"></img>
+				</div>
+				<div>ava + desripiton</div>
+				<div>
+					my posts
+					<div>new post</div>
+					<div>
+						<div>post 1</div>
+						<div>post 2</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}

62e1808d38e7b063a007709ce81a905db5404f56
Nexusov <egor2001grashin@gmail.com>
2021-08-01 03:13:09 +0300

    -render.js + OOP +bind +_callSubscriber +observer

--- src/Redux/State.js
+++ src/Redux/State.js
@@ -1,52 +1,70 @@
-import {rerenderEntireTree} from '../render'
-
-let state = {
-	profilePage: {
-		posts: [
-			{ id: 1, message: 'Hello World!', likesCount: 0 },
-			{ id: 2, message: 'My first post', likesCount: 23 },
-			{ id: 3, message: 'Blabla', likesCount: 2 },
-			{ id: 4, message: 'Da', likesCount: 1 },
-		],
-
-		newPostText: 'Write a new post here'
+let store = {
+	_state: {
+		profilePage: {
+			posts: [
+				{ id: 1, message: 'Hello World!', likesCount: 0 },
+				{ id: 2, message: 'My first post', likesCount: 23 },
+				{ id: 3, message: 'Blabla', likesCount: 2 },
+				{ id: 4, message: 'Da', likesCount: 1 },
+			],
+	
+			newPostText: 'Write a new post here'
+		},
+	
+		dialogsPage: {
+			messages: [
+				{ id: 1, message: 'Hi' },
+				{ id: 2, message: 'Where are you' },
+				{ id: 3, message: 'Bye' },
+				{ id: 4, message: 'I need your bike, bro' },
+				{ id: 5, message: 'Who did it ?' },
+				{ id: 6, message: 'Hei, snakker du norsk ?' },
+			],
+	
+			dialogs: [
+				{ id: 1, name: 'Yegor' },
+				{ id: 2, name: 'Marshak' },
+				{ id: 3, name: 'Ansimist' },
+				{ id: 4, name: 'Kirill' },
+				{ id: 5, name: 'Sito' },
+				{ id: 6, name: 'Mag7' },
+			],
+		}
 	},
 
-    dialogsPage: {
-        messages: [
-            { id: 1, message: 'Hi' },
-            { id: 2, message: 'Where are you' },
-            { id: 3, message: 'Bye' },
-            { id: 4, message: 'I need your bike, bro' },
-            { id: 5, message: 'Who did it ?' },
-            { id: 6, message: 'Hei, snakker du norsk ?' },
-        ],
+	getState() {
+		return this._state
+	},
 
-        dialogs: [
-			{ id: 1, name: 'Yegor' },
-			{ id: 2, name: 'Marshak' },
-			{ id: 3, name: 'Ansimist' },
-			{ id: 4, name: 'Kirill' },
-			{ id: 5, name: 'Sito' },
-			{ id: 6, name: 'Mag7' },
-		],
-    }
-};
+	_callSubscriber() {
+		console.log ('')
+	},
 
-export let addPost = () => {
-	let newPost = {
-		id: 5,
-		message: state.profilePage.newPostText,
-		likesCount: 0,
-	}
-	state.profilePage.posts.push(newPost);
-	state.profilePage.newPostText = ''
-	rerenderEntireTree(state)
+	addPost() {
+		let newPost = {
+			id: 5,
+			message: this._state.profilePage.newPostText,
+			likesCount: 0,
+		}
+		this._state.profilePage.posts.push(newPost);
+		this._state.profilePage.newPostText = ''
+		this._callSubscriber(this._state)
+	},
+
+	updateNewPostText(newText) {
+		this._state.profilePage.newPostText = newText
+		this._callSubscriber(this._state)
+	},
+
+	subscribe(observer) {  
+		this._callSubscriber = observer		  
+	}										  
+	
 }
 
-export let updateNewPostText = (newText) => {
-	state.profilePage.newPostText = newText
-	rerenderEntireTree(state)
-}
 
-export default state;
+/* В state мы не можем импортировать функцию renderEntireTree, но мы можем ее туда передать через subscribe, 
+который вызывается в index.js. После чего, созданная функция renderEntireTree в state переопределяется на ту, 
+которую мы передали в subscribe. И теперь, когда мы добавляем пост, в state вызывается эта функция renderEntireTree */
+
+export default store;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import reportWebVitals from './reportWebVitals';
-import {rerenderEntireTree} from './render'
-import state from './Redux/State'
+import store from './Redux/State'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-rerenderEntireTree(state)
+let rerenderEntireTree = (state) => {
+	ReactDOM.render(
+		<React.StrictMode>
+			<App 
+                state={state} 
+                addPost={store.addPost.bind(store)}
+                updateNewPostText = {store.updateNewPostText.bind(store)} />
+		</React.StrictMode>,
+		document.getElementById('root')
+	);
+};
 
+rerenderEntireTree(store.getState())
+
+store.subscribe(rerenderEntireTree)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals

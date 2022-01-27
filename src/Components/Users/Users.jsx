df98d99906d159b053500bc51ac451167f25435d
Nexus <egor2001grashin@gmail.com>
2022-01-27 04:03:58 +0300

    Side Effect of Class Components

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -4,13 +4,15 @@ import styles from './Users.module.css';
 import userPhoto from '../../Assets/images/user.jpg'
 
 const Users = (props) => {
-    if (props.users.length === 0) {
-
-		axios.get("https://social-network.samuraijs.com/api/1.0/users")
-			.then (response => {props.setUsers(response.data.items)})
-	}
+	let getUsers = () => {
+		if (props.users.length === 0) {
+			axios.get('https://social-network.samuraijs.com/api/1.0/users')
+				.then((response) => {props.setUsers(response.data.items);});
+		}
+	};
     
     return <div>
+		<button onClick = {getUsers}>Get Users</button>
             {
                 props.users.map( u => <div key = {u.id}>
                     <span>

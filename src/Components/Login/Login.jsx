92f117a3f10ebce52da97fb343d00e663d348c7d
Nexus <egor2001grashin@gmail.com>
2022-02-18 01:39:50 +0300

    Formik | Login state managing

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { useFormik } from 'formik'
 
-const Login = () => {
-    return <h1>Login</h1>
+const LoginForm = () => {
+
+    const formik  = useFormik({
+        initialValues: {
+            login: '',
+            password: '',
+            rememberMe: '',
+        }
+    })
+
+    return (
+		<>
+			<h1>Login</h1>
+			<form>
+				<div>
+					<input placeholder = {'Login'} type = 'text' id = 'login' name = {'login'} onChange = {formik.handleChange} value = {formik.values.login} />
+				</div>
+				<div>
+					<input placeholder = {'Password'} type = 'password' id = 'password' name = {'password'} onChange = {formik.handleChange} value = {formik.values.password} />
+				</div>
+				<div>
+					<input type = {'checkbox'} id = 'rememberMe' name = {'rememberMe'} onChange = {formik.handleChange} value = {formik.values.rememberMe} /> remember me
+				</div>
+                <div>
+                    <button>Login</button>
+                </div>
+			</form>
+		</>
+	);
 }
 
-export default Login
+export default LoginForm

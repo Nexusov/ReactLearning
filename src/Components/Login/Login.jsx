e6f4511bfc9f5f6e1279d8aa4165b33f9b732d70
Nexus <egor2001grashin@gmail.com>
2022-02-18 02:30:35 +0300

    FORMIK | Displaying error messages

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,31 +1,57 @@
 import React from "react";
 import { useFormik } from 'formik'
+import s from "./Login.module.css";
+
+const initialValues = {
+    login: '',
+    password: '',
+    rememberMe: '',
+}
+
+const onSubmit = values => {
+    console.log('form data', values)
+}
+
+const validate = values => {
+    let errors = {}
+
+    if (!values.login) {
+        errors.login = 'Required'
+    }
+
+    if (!values.password) {
+        errors.password = 'Required'
+    }
+
+    return errors
+}
 
 const LoginForm = () => {
-
     const formik  = useFormik({
-        initialValues: {
-            login: '',
-            password: '',
-            rememberMe: '',
-        }
+        initialValues,
+        onSubmit,
+        validate
     })
-
+    
     return (
 		<>
-			<h1>Login</h1>
-			<form>
-				<div>
-					<input placeholder = {'Login'} type = 'text' id = 'login' name = {'login'} onChange = {formik.handleChange} value = {formik.values.login} />
+			<h1>Login Page</h1>
+			<form onSubmit = {formik.handleSubmit}>
+				<div className = {s.formControl}>
+                    <label htmlFor = "login">Login</label>
+					<input placeholder = {'Login'} type = 'text' id = 'login' name = {'login'} onChange = {formik.handleChange} onBlur = {formik.handleBlur} value = {formik.values.login} />
+                    {formik.touched.login &&  formik.errors.login ? <div className = {s.error}>{formik.errors.login}</div> : null}
+				</div>
+				<div className = {s.formControl}>
+                <label htmlFor = "password">Password</label>
+                    <input placeholder = {'Password'} type = 'password' id = 'password' name = {'password'} onChange = {formik.handleChange} onBlur = {formik.handleBlur} value = {formik.values.password} />
+					{formik.touched.password && formik.errors.password ? <div className = {s.error}>{formik.errors.password}</div> : null}
 				</div>
 				<div>
-					<input placeholder = {'Password'} type = 'password' id = 'password' name = {'password'} onChange = {formik.handleChange} value = {formik.values.password} />
-				</div>
-				<div>
-					<input type = {'checkbox'} id = 'rememberMe' name = {'rememberMe'} onChange = {formik.handleChange} value = {formik.values.rememberMe} /> remember me
+					<input type = {'checkbox'} id = 'rememberMe' name = {'rememberMe'} onChange = {formik.handleChange} value = {formik.values.rememberMe} /> Remember me
 				</div>
                 <div>
-                    <button>Login</button>
+                    <button type = 'submit'>Login</button>
                 </div>
 			</form>
 		</>
